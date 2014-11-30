5cea512e1967671956b07898a535a85d229ac7f0
Bayarkhuu Purevgaram <bayarkhuu0512@gmail.com>
2014-11-30 22:08:23 +0800

    Settings section added, but toggles left.

--- src/com/mercy/happybaby/ui/others/ProfileFragment.java
+++ src/com/mercy/happybaby/ui/others/ProfileFragment.java
@@ -20,7 +20,7 @@ public class ProfileFragment extends Fragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		// Inflate the layout for this fragment
-		return inflater.inflate(R.layout.timeline_view, container, false);
+		return inflater.inflate(R.layout.profile_view, container, false);
 	}
 
 	@Override
