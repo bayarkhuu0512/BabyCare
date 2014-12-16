36fb1501cdef7e4048afb05067167a191369597f
Bayarkhuu Purevgaram <bayarkhuu0512@gmail.com>
2014-12-16 22:03:08 +0800

    Tooth and Purchase views added.

--- src/com/mercy/happybaby/ui/purchase/AddPurchaseFragment.java
+++ src/com/mercy/happybaby/ui/purchase/AddPurchaseFragment.java
@@ -5,37 +5,52 @@ import java.util.Calendar;
 
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.content.Context;
+import android.graphics.Typeface;
 import android.os.Bundle;
-import android.support.v4.widget.DrawerLayout;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.j256.ormlite.android.apptools.OpenHelperManager;
 import com.j256.ormlite.dao.Dao;
 import com.mercy.happybaby.R;
 import com.mercy.happybaby.db.DatabaseHelper;
-import com.mercy.happybaby.entities.Breast;
 import com.mercy.happybaby.entities.Purchase;
 import com.mercy.happybaby.entities.Timeline;
 import com.mercy.happybaby.ui.timeline.TimelineFragment;
-import com.mercy.happybaby.utils.DateRangeInstance;
+import com.mercy.happybaby.utils.Constants;
 import com.mercy.happybaby.utils.crouton.CroutonMessage;
 import com.mercy.happybaby.utils.crouton.Style;
 
+import dreamers.graphics.RippleDrawable;
+
 public class AddPurchaseFragment extends Fragment {
-	Button leftBreast, rightBreast;
+	String LOG_TAG = AddPurchaseFragment.class.getName();
+
+	private TextView timePurchase;
+	private ListView purchaseList;
+	private String[] purchaseNames;
+
 	private CroutonMessage crtnMsg = null;
 	private Dao<Timeline, Integer> timelineDAO;
 	private DatabaseHelper databaseHelper = null;
-	DateRangeInstance dateRange = DateRangeInstance.getInstance();
+	Calendar cal = Calendar.getInstance();
+	private Typeface roboto_light;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
+		roboto_light = Typeface.createFromAsset(getActivity().getAssets(),
+				Constants.ROBOTO_LIGHT);
+		
 		// Inflate the layout for this fragment
 		View root = inflater.inflate(R.layout.add_purchase, container, false);
 		crtnMsg = new CroutonMessage(getActivity());
@@ -45,40 +60,55 @@ public class AddPurchaseFragment extends Fragment {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
-		Button leftBreast = (Button) root.findViewById(R.id.breastLeft);
-		leftBreast.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				Log.d("AddBreast", "left");
-				addPurchase(false);
-			}
-		});
-
-		Button rightBreast = (Button) root.findViewById(R.id.breastRight);
-		rightBreast.setOnClickListener(new OnClickListener() {
+		timePurchase = (TextView) root.findViewById(R.id.timePurchase);
+		timePurchase.setTypeface(roboto_light);
+		timePurchase.setText(Constants.timeFormat.format(cal.getTime()) + "");
+		timePurchase.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				Log.d("AddBreast", "right");
-				addPurchase(true);
+				Log.d(LOG_TAG, "timeFeed");
 			}
 		});
 
-		Button close = (Button) root.findViewById(R.id.close);
+		purchaseNames = getResources().getStringArray(R.array.vitamin_array);
+		purchaseList = (ListView) root.findViewById(R.id.purchaseList);
+		purchaseList.setAdapter(new PurchaseListAdapter(getActivity(), purchaseNames));
+
+		ImageButton close = (ImageButton) root.findViewById(R.id.close);
+		RippleDrawable.createRipple(close,
+				getResources().getColor(R.color.mainColor));
 		close.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				Log.d("AddBreast", "close");
+				Log.d(LOG_TAG, "close");
 				gotoTimeline();
 			}
 		});
-		
+		ImageButton save = (ImageButton) root.findViewById(R.id.save);
+		RippleDrawable.createRipple(save,
+				getResources().getColor(R.color.mainColor));
+		save.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Log.d(LOG_TAG, "save");
+				// if (isSelectedRight) {
+				// addBreast(true);
+				// } else if (isSelectedLeft) {
+				// addBreast(false);
+				// } else {
+				// crtnMsg.hide();
+				// crtnMsg.showCrouton(Style.CONFIRM, getActivity()
+				// .getResources().getString(R.string.pleaseselect));
+				// }
+			}
+		});
+
 		return root;
 	}
 
@@ -89,8 +119,8 @@ public class AddPurchaseFragment extends Fragment {
 		}
 		return databaseHelper;
 	}
-
-	private void addPurchase(boolean isRight) {
+	
+	private void addVitamin(boolean isRight) {
 		crtnMsg.hide();
 		crtnMsg.showCrouton(Style.INFO,
 				getActivity().getResources().getString(R.string.success));
@@ -111,11 +141,11 @@ public class AddPurchaseFragment extends Fragment {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		dateRange.setEndDate(purchaseCal1.getTime());
+		//dateRange.setEndDate(purchaseCal1.getTime());
 		gotoTimeline();
 	}
-	
-	private void gotoTimeline(){
+
+	private void gotoTimeline() {
 		TimelineFragment fragment = new TimelineFragment();
 		FragmentManager fragmentManager = getFragmentManager();
 		fragmentManager
@@ -125,5 +155,27 @@ public class AddPurchaseFragment extends Fragment {
 						R.animator.slide_down)
 				.replace(R.id.content_frame, fragment).commit();
 		getActivity().getActionBar().show();
+	}
+
+	public class PurchaseListAdapter extends ArrayAdapter<String> {
+
+		public PurchaseListAdapter(Context context, String[] vitaminNames) {
+			// TODO Auto-generated constructor stub
+			super(context, R.layout.add_purchase_item, vitaminNames);
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			LayoutInflater inflater = LayoutInflater.from(getContext());
+			convertView = inflater.inflate(R.layout.add_purchase_item, parent,
+					false);
+			TextView vitaminName = (TextView) convertView
+					.findViewById(R.id.purchaseName);
+			ImageView vitaminIcon = (ImageView) convertView
+					.findViewById(R.id.purchaseIcon);
+			vitaminName.setText(purchaseNames[position] + "");
+			return convertView;
+
+		}
 	}
 }
